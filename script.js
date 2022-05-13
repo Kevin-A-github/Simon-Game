fa5e43f64027dfc7b3cb353edde040e162960721
Kevin <kabreu11385@gmail.COM>
2022-05-13 16:08:04 -0400

    aded styles

---
+++ script.js
@@ -0,0 +1,10 @@
+////////  /HTML Selectors ///////////////////////
+
+const redSquare = document.querySelector('#block1');
+const redGreen = document.querySelector('#block2');
+const redYellow = document.querySelector('#block3');
+const redBlue = document.querySelector('#block4');
+
+//////////////////////////////////////////////////////////////
+
+const startGame = () => {};
