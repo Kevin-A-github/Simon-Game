d800c95b9236f3f9c9732380374e301923f9a6c4
Kevin <kabreu11385@gmail.COM>
2022-05-13 16:54:03 -0400

    clicked function

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ const redSquare = document.querySelector('#block1');
 const redGreen = document.querySelector('#block2');
 const redYellow = document.querySelector('#block3');
 const redBlue = document.querySelector('#block4');
+const buttons = document.querySelectorAll('.grid');
 
 //////////////////////////////////////////////////////////////
 
-const startGame = () => {};
+let sequence = [];
+let playerSequence = [];
+
+const clickedButton = () => {
+  buttons.forEach(button => {
+    button.addEventListener('click', e => {
+      e.preventDefault();
+    });
+  });
+};
+clickedButton();
