cba09cf8b9c85f90ec0deae5202a3a24d1765817
Kevin <kabreu11385@gmail.COM>
2022-05-14 11:08:06 -0400

    added random sequence and user click

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 ////////  /HTML Selectors ///////////////////////
 
-const redSquare = document.querySelector('#block1');
-const redGreen = document.querySelector('#block2');
-const redYellow = document.querySelector('#block3');
-const redBlue = document.querySelector('#block4');
+const redSquare = document.querySelector('#red');
+const redGreen = document.querySelector('#green');
+const redYellow = document.querySelector('#yellow');
+const redBlue = document.querySelector('#blue');
 const buttons = document.querySelectorAll('.grid');
 
 //////////////////////////////////////////////////////////////
 
+let buttonColors = ['red', 'green', 'yellow', 'blue'];
 let sequence = [];
-let playerSequence = [];
+let userSequence = [];
 
 const clickedButton = () => {
   buttons.forEach(button => {
     button.addEventListener('click', e => {
-      e.preventDefault();
+      userSequence.push(button);
+      console.log(userSequence);
     });
   });
 };
 clickedButton();
+
+/////////////// Returns random color ////////////
+const nextSequence = () => {
+  let randomNum = Math.floor(Math.random() * 4);
+  sequence.push(buttonColors[randomNum]);
+};
+nextSequence();
